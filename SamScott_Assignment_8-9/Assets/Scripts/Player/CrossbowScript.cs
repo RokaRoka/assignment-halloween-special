20e57637664e78d32673c453c0f3e267225e4266
samscott <samscott497@gmail.com>
2017-10-26 16:40:38 -0400

    Add Crossbow script and GameManager script. Crossbow follows mouse.

---
+++ SamScott_Assignment_8-9/Assets/Scripts/Player/CrossbowScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossbowScript : MonoBehaviour {
+
+	//reference to camera
+	private Camera _mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		_mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FollowCursor();
+		CheckInput();
+	}
+
+	private void FollowCursor() {
+		//get mouse position and convert to world
+		Vector3 currentMousePosition = Input.mousePosition;
+		currentMousePosition.z = _mainCamera.farClipPlane;
+		//current mouse position is now in world
+		currentMousePosition = _mainCamera.ScreenToWorldPoint(currentMousePosition);
+
+		//adjust weapon rotation
+		transform.LookAt(currentMousePosition, Vector3.up);
+	}
+
+	private void CheckInput() {
+		//check for click
+
+		//if click, do ray on next fixed frame
+	}
+}
---
+++ SamScott_Assignment_8-9/Assets/Scripts/Player/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameMode {
+	Game, Pause
+}
+
+public class GameManager : MonoBehaviour {
+
+	public GameMode currentMode;
+
+	private void Awake() {
+		/*/switch mode to game mode
+		SwitchMode(GameMode.Game);
+		/*/
+	}
+
+	private void Start() {
+		//switch mode to game mode
+		SwitchMode(GameMode.Game);
+	}
+
+	public void SwitchMode(GameMode newMode) {
+		currentMode = newMode;
+		//if game, resume play and confine cursor
+		if (newMode == GameMode.Game) {
+			Cursor.lockState = CursorLockMode.Confined;
+			Debug.Log("Mode is Game!");
+		}
+		//if pause, stop play and free cursor
+		//else if (newMode == GameMode.Pause) Cursor.lockState = CursorLockMode.None;
+	}
+}
